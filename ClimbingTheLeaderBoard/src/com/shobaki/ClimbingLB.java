0b72e33bf8b67eb85c7ea402146be844ea7503e7
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-05-28 12:38:53 -0400

    8th commit

--- ClimbingTheLeaderBoard/src/com/shobaki/ClimbingLB.java
+++ ClimbingTheLeaderBoard/src/com/shobaki/ClimbingLB.java
@@ -8,69 +8,94 @@ public class ClimbingLB {
         int[] aliceScores = new int[]{5, 25, 50, 120};//ASC
         System.out.println(Arrays.toString(climbingLeaderboard(scores, aliceScores)));
         //[6, 4, 2, 1] the right answer
-
+//
         int[] scores1 = new int[]{100, 90, 90, 80, 75, 60};//DESC
         int[] aliceScores1 = new int[]{50, 65, 77, 90, 102};//ASC
         System.out.println(Arrays.toString(climbingLeaderboard(scores1, aliceScores1)));
         //[6, 5, 4, 2, 1] the right answer
+//
+        int[] scores2 = new int[]{100, 90, 90, 80, 75, 60};//DESC
+        int[] aliceScores2 = new int[]{85, 90, 90, 100, 100, 102, 102};//ASC
+        System.out.println(Arrays.toString(climbingLeaderboard(scores2, aliceScores2)));
+        //[3, 2, 2, 1, 1, 1, 1]
     }
+    // 9/12 test cases failed :(    HackerRank
 
     public static int[] climbingLeaderboard(int[] scores, int[] alice) {
 
         int[] result = new int[alice.length];
-        int position = 1;
+        int position = 0;
         int start = 0;
-        int count=0;
+        int count = 0;
         for (int i = alice.length - 1; i >= 0; i--) {
 
             for (int j = start; j < scores.length; j++) {
 
-                if(i < alice.length-1){
-                    if(alice[i] == alice[i+1]){
+                if (alice[i] >= scores[0]) {
+                    result[i] = 1;
+                    break;
+                }
+
+                if (j != scores.length - 1) {
+                    position++;
+                    if (j > 0) {
+                        if (scores[j] == scores[j - 1]) {
+                            position--;
+                        }
+                    }
+                    if (alice[i] < scores[j] && alice[i] > scores[j + 1]) {
+                        count = 0;
                         result[i] = position;
+                        start = j+1;
                         break;
                     }
-                }
-                if (alice[i] > scores[0]) {
-                    result[i] = 1;
-                    start++;
-                    break;
-                }
-
-                if (alice[i] == scores[j]) {
-                    count++;
-                    if(count>1){
-                        continue;
-                    }
-                    result[i] = position;
-                    start = j;
-                    position++;
-                    break;
-                }
-
-                if (j == scores.length - 1) {
-                    if (alice[i] < scores[j]) {
-                        result[i] = position + 1;
+                    if (alice[i] == scores[j]) {
+                        count++;
+                        if (count > 1) {
+                            position--;
+                            result[i] = position;
+                            start = j;
+                            break;
+                        }
+                        result[i] = position;
+                        start = j;
                         break;
                     }
                 }else{
-                    if (alice[i] < scores[j]) {
-                        count = 0;
-                        if (scores[j] != scores[j-1]) {
-                            position++;
-                        }
-                      continue;
+                    if(alice[i] >= scores[j]){
+                        result[i] = position;
+                        break;
                     }
-                }
-                if (alice[i] > scores[j]) {
-                    count =0;
-                    result[i] = position;
-                    start = j;
-                    //position++;
+                    result[i] = position+1;
                     break;
                 }
+
             }
         }
         return result;
     }
 }
+
+
+//
+//                if (j == scores.length - 1) {
+//                    if (alice[i] < scores[j]) {
+//                        result[i] = position + 1;
+//                        break;
+//                    }
+//                }else{
+//                    if (alice[i] < scores[j]) {
+//                        count = 0;
+//                        if (scores[j] != scores[j-1]) {
+//                            position++;
+//                        }
+//                      continue;
+//                    }
+//                }
+//                if (alice[i] > scores[j]) {
+//                    count =0;
+//                    result[i] = position;
+//                    start = j;
+//                    //position++;
+//                    break;
+//                }
--- ClimbingTheLeaderBoard/src/com/shobaki/Main.java
+++ ClimbingTheLeaderBoard/src/com/shobaki/Main.java
@@ -44,7 +44,6 @@ public class Main {
             d++;
         }
 
-
         sortAndReserved(newScores);
 
 
@@ -63,7 +62,6 @@ public class Main {
 
             h++;
         }
-
 
         return positions.get(getPos(newScores, score));
 
