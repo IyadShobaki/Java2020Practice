ad622e5cc7c72a7cc8ba0abdb7f8ac2a5705ff7e
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-05-29 14:11:24 -0400

    10th commit

--- 001-HackerRank/src/com/shobaki/RepeatedString.java
+++ 001-HackerRank/src/com/shobaki/RepeatedString.java
@@ -4,31 +4,79 @@ public class RepeatedString {
     public static void main(String[] args) {
         String string = "aba";
         System.out.println(repeatedString(string, 10));  //should be 7
+        System.out.println(10 /string.length());
+        System.out.println(10%string.length());
 
-        try{
-            String string1 = "a";
-            System.out.println(repeatedString(string1, 1000000000000L));//should be 1000000000000
-        }catch (OutOfMemoryError e){
-            e.printStackTrace();
-        }
+
+        String string1 = "a";
+        System.out.println(repeatedString(string1, 1000000000000L));//should be 1000000000000
 
 
         String string2 = "abcac";
         System.out.println(repeatedString(string2, 10));//should be 4
+
+        String string3 = "aadcdaccacabdaabadadaabacdcbcacabbbadbdadacbdadaccbbadbdcadbdcacacbcacddbcbbbaaccbaddcabaacbcaabbaaa";
+        System.out.println(repeatedString(string3, 942885108885L));//should be 330009788107
+
+        System.out.println(942885108885L / string3.length());
+        System.out.println(942885108885L % string3.length());
+        System.out.println(string3.length());
     }
 
     public static long repeatedString(String s, long n) {
-        while(s.length() <n ){
-            s = s.concat(s);
 
-        }
-        System.out.println(s);
-        long count =0;
-        for(int i =0; i < n; i++){
-            if(s.charAt(i) == 'a'){
-                count++;
+        long aCount = 0L;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'a') {
+                aCount++;
             }
         }
-        return count;
+
+        long l = n / s.length();
+        long p = aCount * l ;
+        for(int i=0; i< (n%s.length()); i++){
+            if(s.charAt(i) == 'a'){
+                p++;
+            }
+        }
+
+        return p;
+
+//
+//        while (s.length() < n) {
+//            s = s.concat(s);
+//
+//        }
+//        System.out.println(s);
+//        long count = 0L;
+//        for (int i = 0; i < n; i++) {
+//            if (s.charAt(i) == 'a') {
+//                count++;
+//            }
+//        }
+//        return count;
+        }
     }
-}
+
+
+    //internet solution
+//        char[] chrIn = s.toCharArray();
+//        int i = 0;
+//        long max = 0;
+//        long quo = n / chrIn.length;
+//        long rem = n % chrIn.length;
+//        while (i < chrIn.length) {
+//            if (chrIn[i] == 'a') {
+//                max++;
+//            }
+//            i++;
+//        }
+//        i = 0;
+//        max = max * quo;
+//        while (i < rem) {
+//            if (chrIn[i] == 'a') {
+//                max++;
+//            }
+//            i++;
+//        }
+//        return max;
