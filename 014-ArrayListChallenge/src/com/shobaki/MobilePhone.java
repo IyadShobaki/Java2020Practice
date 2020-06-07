daedcb460fe7efeb19cb1d5ac69f0c81b784b740
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-06-06 21:47:30 -0400

    20th commit

--- 014-ArrayListChallenge/src/com/shobaki/MobilePhone.java
+++ 014-ArrayListChallenge/src/com/shobaki/MobilePhone.java
@@ -19,6 +19,30 @@ public class MobilePhone {
         contacts.add(contact);
         return true;
     }
+    public boolean updateContact(Contact oldContact, Contact newContact){
+        int foundPosition = findContact(oldContact);
+        if(foundPosition < 0){
+            System.out.println(oldContact.getName() + ", was not found.");
+            return false;
+        }else if(findContact(newContact.getName()) != -1){
+            System.out.println("Contact with name " + newContact.getName() +
+                    " already exists. Update was not successful.");
+            return false;
+        }
+        this.contacts.set(foundPosition, newContact);
+        System.out.println(oldContact.getName() + ", was replaced with " + newContact.getName());
+        return true;
+    }
+    public boolean removeContact(Contact contact){
+        int foundPosition = findContact(contact);
+        if(foundPosition < 0){
+            System.out.println(contact.getName() + ", was not found.");
+            return false;
+        }
+        this.contacts.remove(foundPosition);
+        System.out.println(contact.getName() + ", was deleted.");
+        return true;
+    }
     private int findContact(Contact contact){
         return contacts.indexOf(contact);
     }
@@ -31,25 +55,26 @@ public class MobilePhone {
         }
         return -1;
     }
+    public String queryContact(Contact contact){
+        if(findContact(contact) >= 0){
+            return contact.getName();
+        }
+        return null;
+    }
+    public Contact queryContact(String name){
+        int position = findContact(name);
+        if(position >=0 ){
+            return this.contacts.get(position);
+        }
+        return null;
+    }
 
-    public void printContactsList(){
-        System.out.println("You have " + contacts.size() + " contact in your list.");
+    public void printContacts(){
+        System.out.println("Contact List");
         for(int i =0; i<contacts.size(); i++){
-            System.out.println((i+1) + ". " + contacts.get(i).getName() + ", phone number: " +
+            System.out.println((i+1) + ". " + contacts.get(i).getName() + " -> " +
                     contacts.get(i).getPhoneNumber());
         }
     }
 
-
-    public void modifyContact(String currentContact, Contact newContact){
-        int position = findContact(currentContact);
-        if(position >= 0){
-            modifyContact(position, newContact);
-        }
-    }
-    public void modifyContact(int position, Contact newContact){
-        contacts.set(position, newContact);
-        System.out.println("Contact " + (position +1) + " has been modified");
-
-    }
 }
